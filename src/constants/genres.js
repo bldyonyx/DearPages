88253178042c2412902d5e7f2a283f945a8ee78f
bldyonyx <maya.akima@gmail.com>
2026-09-21 20:03:06 +0200

    feat: add onboarding genre options

---
+++ src/constants/genres.js
@@ -0,0 +1,66 @@
+export const AVAILABLE_GENRES = [
+  {
+    label: 'Fantasy',
+    subject: 'fantasy',
+  },
+  {
+    label: 'Romance',
+    subject: 'romance',
+  },
+  {
+    label: 'Mystère',
+    subject: 'mystery',
+  },
+  {
+    label: 'Thriller',
+    subject: 'thriller',
+  },
+  {
+    label: 'Policier',
+    subject: 'crime',
+  },
+  {
+    label: 'Horreur',
+    subject: 'horror',
+  },
+  {
+    label: 'Science-fiction',
+    subject: 'science fiction',
+  },
+  {
+    label: 'Aventure',
+    subject: 'adventure',
+  },
+  {
+    label: 'Young Adult',
+    subject: 'young adult',
+  },
+  {
+    label: 'Classiques',
+    subject: 'classics',
+  },
+  {
+    label: 'Histoire',
+    subject: 'history',
+  },
+  {
+    label: 'Biographies',
+    subject: 'biography',
+  },
+  {
+    label: 'Poésie',
+    subject: 'poetry',
+  },
+  {
+    label: 'Bandes dessinées',
+    subject: 'comics',
+  },
+  {
+    label: 'Développement personnel',
+    subject: 'self help',
+  },
+  {
+    label: 'Philosophie',
+    subject: 'philosophy',
+  },
+]
